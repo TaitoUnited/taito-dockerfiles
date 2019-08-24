42fa0371262be4b09e8b862dfc4a209761b639ac
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-08-24 21:51:32 +0300

    fix: sqitch requires cpanminus at runtime

--- taito-cli-deps/tools/install-sqitch.sh
+++ taito-cli-deps/tools/install-sqitch.sh
@@ -5,7 +5,7 @@ set -eux; \
     apt-get -qqy install build-essential perl cpanminus libdbd-pg-perl libdatetime-perl libclone-perl \
       libperlio-utf8-strict-perl libhash-merge-perl && \
     cpanm --quiet --notest App::Sqitch && \
-    apt-get -qqy --purge remove cpanminus build-essential && \
+    apt-get -qqy --purge remove build-essential && \
     apt-get -qqy --purge autoremove && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
