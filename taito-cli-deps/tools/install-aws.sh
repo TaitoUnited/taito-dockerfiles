71f4c9cc855c03f9e98f779118874008fd625b3d
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-08-25 13:20:43 +0300

    chore: env AWS_VERSION

--- taito-cli-deps/tools/install-aws.sh
+++ taito-cli-deps/tools/install-aws.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+
 set -eux; \
     cd /tmp && \
     apt-get -qqy update && \
@@ -6,7 +7,7 @@ set -eux; \
     curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip" && \
     unzip awscli-bundle.zip && \
     ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws && \
-    curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.12.7/2019-03-27/bin/linux/amd64/aws-iam-authenticator && \
+    curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/${AWS_VERSION}/bin/linux/amd64/aws-iam-authenticator && \
     chmod +x ./aws-iam-authenticator && \
     mv ./aws-iam-authenticator /usr/local/bin/aws-iam-authenticator && \
     rm -rf awscli* && \
