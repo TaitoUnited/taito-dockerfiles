4207377083f307e6822915997f76c5a0785737a1
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-09-03 22:40:46 +0300

    feat: moved taito-cli-deps elsewhere

--- taito-cli-deps/tools/install-bats.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-set -eux; \
-    git clone https://github.com/bats-core/bats-core.git && \
-    ./bats-core/install.sh /usr/local && \
-    rm -rf bats-core
--- taito-cli-deps/tools/install-db-clients.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-set -eux; \
-    apt-get -qqy update && \
-    apt-get -qqy install postgresql-client default-mysql-client && \
-    apt-get clean && \
-    rm -rf /var/lib/apt/lists/*
